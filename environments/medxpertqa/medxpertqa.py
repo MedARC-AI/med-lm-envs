d0a4ea2f51007d0b8e465641bd68f774720d55b8
Benjamin Warner <me@benjaminwarner.dev>
2025-10-16 23:07:41 -0400

    bug fix and ruff format

--- environments/medxpertqa/medxpertqa.py
+++ environments/medxpertqa/medxpertqa.py
@@ -5,20 +5,20 @@ from datasets import load_dataset
 import os
 import re
 
+
 def _get_system_prompt(use_think: bool) -> str:
     think_system_prompt = (
         "You are a helpful medical assistant. Think step-by-step inside <think>...</think> tags."
         "Put your final answer within \boxed{{}}.\nQ:"
-        )
-    no_think_system_prompt = (
-        "You are a helpful medical assistant."
-        "Put your final answer within \boxed{{}}.\nQ:"
-        )
+    )
+    no_think_system_prompt = "You are a helpful medical assistant.Put your final answer within \boxed{{}}.\nQ:"
     system_prompt = think_system_prompt if use_think else no_think_system_prompt
     return system_prompt
 
+
 def _build_prompt(question: str, use_think: bool) -> str:
     return _get_system_prompt(use_think) + " " + question
+
 
 def load_environment(
     use_think: bool = False,
@@ -45,7 +45,6 @@ def load_environment(
             "answer": a,
         }
 
-
     mapped = test_dataset.map(_map).filter(lambda r: r is not None)
 
     async def medxpertqa_reward_func(
@@ -58,21 +57,17 @@ def load_environment(
         Returns 1.0 if they match (case-insensitive), else 0.0.
         """
         parser = vf.ThinkParser(extract_boxed_answer) if use_think else vf.Parser(extract_boxed_answer)
-        final_answer = parser.parse(completion).strip()
+        final_answer = parser.parse_answer(completion).strip()
         if final_answer.lower() == answer.lower():
             return 1.0
         else:
             return 0.0
-        
+
     rubric = vf.Rubric(
         funcs=[medxpertqa_reward_func],
         weights=[1.0],
     )
 
-    vf_env = vf.SingleTurnEnv(
-        eval_dataset=mapped,
-        system_prompt=None,
-        rubric=rubric
-    )
+    vf_env = vf.SingleTurnEnv(eval_dataset=mapped, system_prompt=None, rubric=rubric)
 
     return vf_env
